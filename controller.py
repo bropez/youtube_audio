913c36592b50d1e9d4c618fa0ab5e73beb454934
Matthew <matthewbropez@gmail.com>
2020-03-08 16:24:28 -0400

    Renamed save.py file and simplified things
    After a sleep I found out that pytube already has a function to just get the audio so no ffmpeg needed

--- controller.py
+++ controller.py
@@ -1,16 +1,26 @@
-import moviepy.editor as mp
 import os
-import shutil
 
-from pytube3save1video import save_vid
+from save import save_vid, save_audio
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-save_vid(BASE_DIR)
+def download_audio(links):
+	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	for link in links:
+		# save_vid(link, BASE_DIR)
+		save_audio(link, BASE_DIR)
+	
+	
+if __name__ == "__main__":
+	links = [
+		"https://www.youtube.com/watch?v=uX3IZ80dXsU", 
+		"https://www.youtube.com/watch?v=q6cDn9sMRME", 
+		"https://www.youtube.com/watch?v=1w_uufUG9Ac",
+		"https://www.youtube.com/watch?v=22lhyM5zMHQ",
+		"https://www.youtube.com/watch?v=84nwUEIMQr8",
+		"https://www.youtube.com/watch?v=ekgPDRTyriY",
+		"https://www.youtube.com/watch?v=sPy5ospxyPE",
+		"https://www.youtube.com/watch?v=jPCLhSseIb8",
+		"https://www.youtube.com/watch?v=sSts_WOEE-s",
+	]
+	download_audio(links)
 
-title = os.listdir(os.path.join(BASE_DIR, "video\\"))[0]
-with mp.VideoFileClip(os.path.join(BASE_DIR, "video\\{}".format(title))) as clip:
-	clip.audio.write_audiofile(os.path.join(BASE_DIR, "audios\\{}.mp3").format(title.replace('.mp4', '')))
-
-print("deleting placeholder dir")
-shutil.rmtree(os.path.join(BASE_DIR, "video"))
---
+++ save.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+import os
+
+
+def save_vid(video_link, dir):
+	# yt = YouTube("https://www.youtube.com/watch?v=zQA-vg43H48")
+	yt = YouTube(video_link)
+	stream = yt.streams.first()
+	stream.download(os.path.join(dir, "video"))
+	
+	
+def save_audio(video_link, dir):
+	yt = YouTube(video_link)
+	title = yt.title
+	while(title == "YouTube"):
+		yt = YouTube(video_link)
+		title = yt.title
+	print("Now downloading '{}".format(title))
+	audio = yt.streams.get_audio_only()
+	audio.download(os.path.join(dir, "audio_only"))
+	
+
+if __name__ == "__main__":
+	save_audio("https://www.youtube.com/watch?v=uX3IZ80dXsU", "")
